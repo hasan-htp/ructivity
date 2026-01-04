31ed154738bdc0a9d40627f97a2bad5735796043
hasan <hasan.hatipoglu.a@gmail.com>
2026-01-04 23:29:11 +0100

    basic evdev event fetch

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ructivity
 Linux activity tracker daemon in Rust
+
+sudo usermod -aG input $USER
+
+log out 
+
+groups
+
+example: 
+cargo run "/dev/input/by-id/usb-SteelSeries_SteelSeries_Apex_5-event-kbd"
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use std::thread;
+//use evdev::{Device, InputEventKind, Key};
+use evdev::{Device, InputEventKind};
+use std::env;
+
+fn main() -> std::io::Result<()>{
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: cargo run <input_device_path>");
+        return Ok(());
+    }
+
+    let path = &args[1];
+    let mut device = Device::open(path)?;
+
+    let worker_thread = thread::spawn(move || {
+        println!("worker_thread:");
+        loop {
+            let events = match device.fetch_events() {
+                Ok(ev) => ev,
+                Err(e) => {
+                    eprintln!("fetch_events failed: {}", e);
+                    break;
+                }
+            };
+
+            for ev in events {
+                if let InputEventKind::Key(key) = ev.kind() {
+                    // let's print only when a key released
+                    if ev.value() == 0{
+                        println!("{:?}", key);
+                    }
+                }
+            }
+        }
+    });
+
+    match worker_thread.join() {
+        Ok(_) => println!("thread ok"),
+        Err(e) => eprintln!("thread panicked: {:?}", e),
+    }
+
+    Ok(())
 }
