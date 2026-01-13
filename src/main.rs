8de9d9163356fbaa145a1f7dd28a227bd41038b3
hasan <hasan.hatipoglu.a@gmail.com>
2026-01-14 00:23:32 +0100

    log thread and mpsc channel to receive events from key event thread

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-use std::thread;
-//use evdev::{Device, InputEventKind, Key};
-use evdev::{Device, InputEventKind};
-use std::env;
 use chrono::Utc;
+use evdev::{Device, InputEventKind, Key};
+
+use std::env;
 use std::fs::File;
 use std::io::Write;
+use std::sync::mpsc;
+use std::sync::mpsc::{Sender, Receiver};
+use std::thread;
+
+struct Entry {
+    key : Key,
+    time_stamp : String, //TODO: use a struct and format it in log_writer_thread
+}
 
 fn main() -> std::io::Result<()>{
     let args: Vec<String> = env::args().collect();
@@ -19,8 +26,9 @@ fn main() -> std::io::Result<()>{
     let outputpath = &args[2];
     let mut log_file = File::create(outputpath)?;
     
-    let worker_thread = thread::spawn(move ||  -> std::io::Result<()> {
-        println!("worker_thread:");
+    let (tx,rx): (Sender<Entry>, Receiver<Entry>) = mpsc::channel();
+
+    let key_event_thread = thread::spawn(move ||  -> std::io::Result<()> {
         loop {
             let events = match device.fetch_events() {
                 Ok(ev) => ev,
@@ -34,20 +42,48 @@ fn main() -> std::io::Result<()>{
                 if let InputEventKind::Key(key) = ev.kind() {
                     // let's print only when a key released (1 on pressed, 0 on released)
                     if ev.value() == 0 {
-                        let now= Utc::now();
-                        let line = format!("{} {:?}", now.format("%Y-%m-%d %H:%M:%S.%6f: "), key);
-                        writeln!(log_file, "{}", line)?;
-                        println!("{}", line);
+                        let utc_now: chrono::DateTime<Utc>= Utc::now();
+                        let entry = Entry{
+                            key: key,
+                            time_stamp: format!("{}",utc_now.format("%Y-%m-%d %H:%M:%S.%6f")),
+                        };
+                        match tx.send(entry) {
+                            Ok(()) => {}
+                            Err(e) => {
+                                eprintln!("failed to send: {}",e);
+                            }
+                        }                    
                     }
                 }
             }
         }
     });
 
-    match worker_thread.join() {
-        Ok(_) => println!("thread ok"),
-        Err(e) => eprintln!("thread panicked: {:?}", e),
+    let log_writer_thread = thread::spawn(move || -> std::io::Result<()> {
+        loop {
+            match  rx.recv() {
+                Ok(entry) => {
+                    let line = format!("{}, {:?}", entry.time_stamp, entry.key);
+                    writeln!(log_file, "{}", line)?;
+                    println!("{}", line);
+                }
+                Err(e) => {
+                    eprintln!("{}", e);
+                }
+            }
+        }
+    });
+
+    match key_event_thread.join() {
+        Ok(_) => println!("key_event_thread ok"),
+        Err(e) => eprintln!("key_event_thread panicked: {:?}", e),
     }
 
+    match log_writer_thread.join() {
+        Ok(_) => println!("log_writer_thread ok"),
+        Err(e) => eprintln!("log_writer_thread panicked: {:?}", e),
+    }
+    
     Ok(())
+
 }
