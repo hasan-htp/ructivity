3352f483452fe9b4c47114d11db44c8ce71f91ad
hasan <hasan.hatipoglu.a@gmail.com>
2026-01-27 00:05:31 +0100

    update evdev

--- src/event_listener.rs
+++ src/event_listener.rs
@@ -1,5 +1,5 @@
 use chrono::Utc;
-use evdev::{Device, InputEventKind, Key, RelativeAxisType};
+use evdev::{Device, EventSummary, KeyCode, RelativeAxisCode};
 use std::fs;
 use std::sync::mpsc::Sender;
 use std::thread::{self, JoinHandle};
@@ -7,30 +7,31 @@ use std::thread::{self, JoinHandle};
 const EVDEV_INPUT_PATH: &str = "/dev/input/";
 
 pub struct Entry {
-    pub key: Key,
+    pub key: KeyCode,
     pub time_stamp: String, //TODO: use a struct and format it in log_writer_thread
 }
 
 fn is_keyboard(dev: &Device) -> bool {
     if let Some(keys) = dev.supported_keys() {
-        let required_keys = [Key::KEY_ENTER, Key::KEY_SPACE];
+        let required_keys = [KeyCode::KEY_ENTER, KeyCode::KEY_SPACE];
         return required_keys.iter().all(|k| keys.contains(*k));
     }
     false
 }
 
 fn is_mouse(dev: &Device) -> bool {
-        if let Some(axes) = dev.supported_relative_axes() {
-        let required_axes = [RelativeAxisType::REL_X, RelativeAxisType::REL_Y];
+    if let Some(axes) = dev.supported_relative_axes() {
+        let required_axes = [RelativeAxisCode::REL_X, RelativeAxisCode::REL_Y];
         return required_axes.iter().all(|k| axes.contains(*k));
     }
     // TODO add buttons
     false
 }
 
-fn get_devices<DeviceCheck>(is_device:DeviceCheck,inputpath: &str) -> std::io::Result<Vec<Device>> 
-where 
-DeviceCheck : Fn(&Device) -> bool{
+fn get_devices<DeviceCheck>(is_device: DeviceCheck, inputpath: &str) -> std::io::Result<Vec<Device>>
+where
+    DeviceCheck: Fn(&Device) -> bool,
+{
     let mut devices = Vec::new();
 
     for entry in fs::read_dir(inputpath)? {
@@ -54,14 +55,14 @@ DeviceCheck : Fn(&Device) -> bool{
 }
 
 pub fn event_listener(tx: Sender<Entry>) -> std::io::Result<Vec<JoinHandle<std::io::Result<()>>>> {
-    let keyboards = get_devices(is_keyboard,EVDEV_INPUT_PATH)?;
-    println!("{} keyboard evdev found:", keyboards.len());
+    let keyboards = get_devices(is_keyboard, EVDEV_INPUT_PATH)?;
+    println!("{} keyboard evdev found", keyboards.len());
 
-    let mice = get_devices(is_mouse,EVDEV_INPUT_PATH)?;
-    println!("{} mouse evdev found:", mice.len());
+    let mice = get_devices(is_mouse, EVDEV_INPUT_PATH)?;
+    println!("{} mouse evdev found", mice.len());
     for dev in mice {
         let dev_name = dev.name().unwrap_or("unknown").to_string();
-        println!("mouse device name:{}", dev_name);
+        println!("mouse device name{}", dev_name);
     }
 
     let mut handles = Vec::new();
@@ -82,18 +83,22 @@ pub fn event_listener(tx: Sender<Entry>) -> std::io::Result<Vec<JoinHandle<std::
                 };
 
                 for ev in events {
-                    if let InputEventKind::Key(key) = ev.kind() {
-                        if ev.value() == 0 {
-                            let utc_now = Utc::now();
-                            let entry = Entry {
-                                key,
-                                time_stamp: utc_now.format("%Y-%m-%d %H:%M:%S.%6f").to_string(),
-                            };
+                    match ev.destructure() {
+                        EventSummary::Key(_keyevent, key, value) => {
+                            if value == 0 {
+                                let utc_now = Utc::now();
+                                let entry = Entry {
+                                    key: key, 
+                                    time_stamp: utc_now.format("%Y-%m-%d %H:%M:%S.%6f").to_string(),
+                                };
 
-                            if let Err(e) = tx.send(entry) {
-                                eprintln!("failed to send: {}", e);
+                                if let Err(e) = tx.send(entry) {
+                                    eprintln!("failed to send: {}", e);
+                                }
                             }
                         }
+                        // TODO add relative movements etc.
+                        _ => {}
                     }
                 }
             }
