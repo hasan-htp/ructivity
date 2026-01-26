582970233e4e7d182dd9702c616fc5b67346e572
hasan <hasan.hatipoglu.a@gmail.com>
2026-01-26 23:47:15 +0100

    check relative supported axes for mouse

--- src/event_listener.rs
+++ src/event_listener.rs
@@ -1,5 +1,5 @@
 use chrono::Utc;
-use evdev::{Device, InputEventKind, Key};
+use evdev::{Device, InputEventKind, Key, RelativeAxisType};
 use std::fs;
 use std::sync::mpsc::Sender;
 use std::thread::{self, JoinHandle};
@@ -20,14 +20,18 @@ fn is_keyboard(dev: &Device) -> bool {
 }
 
 fn is_mouse(dev: &Device) -> bool {
-    if let Some(relatic_axis) = dev.supported_relative_axes() {
-        return true;
+        if let Some(axes) = dev.supported_relative_axes() {
+        let required_axes = [RelativeAxisType::REL_X, RelativeAxisType::REL_Y];
+        return required_axes.iter().all(|k| axes.contains(*k));
     }
+    // TODO add buttons
     false
 }
 
-fn get_keyboards(inputpath: &str) -> std::io::Result<Vec<Device>> {
-    let mut keyboards = Vec::new();
+fn get_devices<DeviceCheck>(is_device:DeviceCheck,inputpath: &str) -> std::io::Result<Vec<Device>> 
+where 
+DeviceCheck : Fn(&Device) -> bool{
+    let mut devices = Vec::new();
 
     for entry in fs::read_dir(inputpath)? {
         let entry = entry?;
@@ -40,23 +44,31 @@ fn get_keyboards(inputpath: &str) -> std::io::Result<Vec<Device>> {
         }
 
         if let Ok(dev) = Device::open(&path) {
-            if is_keyboard(&dev) {
-                keyboards.push(dev);
+            if is_device(&dev) {
+                devices.push(dev);
             }
         }
     }
 
-    Ok(keyboards)
+    Ok(devices)
 }
 
 pub fn event_listener(tx: Sender<Entry>) -> std::io::Result<Vec<JoinHandle<std::io::Result<()>>>> {
-    let keyboards = get_keyboards(EVDEV_INPUT_PATH)?;
+    let keyboards = get_devices(is_keyboard,EVDEV_INPUT_PATH)?;
     println!("{} keyboard evdev found:", keyboards.len());
+
+    let mice = get_devices(is_mouse,EVDEV_INPUT_PATH)?;
+    println!("{} mouse evdev found:", mice.len());
+    for dev in mice {
+        let dev_name = dev.name().unwrap_or("unknown").to_string();
+        println!("mouse device name:{}", dev_name);
+    }
+
     let mut handles = Vec::new();
 
     for mut dev in keyboards {
         let dev_name = dev.name().unwrap_or("unknown").to_string();
-        println!("device name:{}", dev_name);
+        println!("keyboard device name:{}", dev_name);
 
         let tx = tx.clone();
         let handle = thread::spawn(move || -> std::io::Result<()> {
